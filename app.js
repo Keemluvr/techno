4a40677c15f01e52734d3bac1c8c3b3645880215
Keila <keiladaianefernandes24@gmail.com>
2020-03-02 20:45:14 -0300

    :white_check_mark: API estática dos produtos para testes terminada

---
+++ app.js
@@ -0,0 +1,20 @@
+const vm = new Vue({
+
+    el: "#app",
+
+    data: {
+        produtos: []
+    },
+
+    methods: {
+        fetchProdutos() {
+            fetch('./api/produtos.json')
+                .then( response => response.json())
+                .then( json => this.produtos = json)
+        }
+    },
+    
+    created() {
+        this.fetchProdutos()
+    },
+})
