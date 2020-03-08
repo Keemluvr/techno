6f54dadd0c4202a5f792dc38ec45421ab8a1807f
Keila <keiladaianefernandes24@gmail.com>
2020-03-07 22:43:01 -0300

    :sparkles: Formatando o preço com filters

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ const vm = new Vue({
         produtos: []
     },
 
+    filters: {
+        numeroPreco(valor) {
+            const formatter = new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL' 
+            });
+            return formatter.format(valor);
+        }
+    },
+
     methods: {
+        // Puxa os produtos 
         fetchProdutos() {
             fetch('./api/produtos.json')
                 .then( response => response.json())
